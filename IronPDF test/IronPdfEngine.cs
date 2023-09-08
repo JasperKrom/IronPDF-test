9b0fea2e021fc42e5a63b570d8df1d21ed1b5d62
JasperKrom <jasper@rapide.software>
2023-09-08 13:44:16 +0200

    Add project files.

---
+++ IronPDF test/IronPdfEngine.cs
@@ -0,0 +1,45 @@
+﻿using IronPdf.Editing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole.PDF
+{
+    public class IronPDFEngine
+    {
+        public void StampPDF()
+        {
+            var htmlStamper = File.ReadAllText("../../../../test74480.html");
+
+            var stamper =
+            new HtmlStamper(htmlStamper)
+            {
+                HorizontalAlignment = HorizontalAlignment.Left,
+                VerticalAlignment = VerticalAlignment.Top,
+                IsStampBehindContent = false,
+                Opacity = 100,
+                VerticalOffset = new Length(25),
+                HorizontalOffset = new Length(25),
+                Scale = 75,
+            };
+
+            var pdf = PdfDocument.FromFile("../../../../Hey.pdf");
+
+            pdf.ApplyStamp(stamper);
+
+            pdf.SaveAs("../../../../stampedimageBasic.pdf");
+        }
+    
+        public void LockPDF()
+        {
+            var pdf = PdfDocument.FromFile("../../../../TestWithForm.pdf");
+
+            pdf.SecuritySettings.OwnerPassword = "password";
+            pdf.SecuritySettings.MakePdfDocumentReadOnly("password");
+
+            pdf.SaveAs("../../../../formlocked.pdf");
+        }
+    }
+}
---
+++ IronPDF test/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TestConsole.PDF;
+
+var ironEngine = new IronPDFEngine();
+
+ironEngine.StampPDF();
+ironEngine.LockPDF();
+
+Console.WriteLine("Done");
+Console.ReadLine();
